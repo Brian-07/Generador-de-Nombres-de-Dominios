dcaa99ea58f3abcdae6f2426c85aea43a4a8d30a
Brian-07 <sabbatella66@gmail.com>
2022-09-24 09:13:08 +0000

    Trabajo terminado

---
+++ app.js
@@ -0,0 +1,26 @@
+var pronoun = ['the','our'];
+var adj = ['great', 'big' ];
+var noun = ['jogger','racoon',`pued`];
+var dominio = [`.com`,`.es`,`.eu`]
+
+pronoun.forEach(function(elemento){
+    adj.forEach(function (elemento1){
+        noun.forEach(function (elemento2){
+            dominio.forEach(function (elemento3){
+                console.log(elemento+ elemento1 + elemento2 + elemento3);
+           });
+         });
+    });
+});
+// for(let elemento of pronoun){ 
+//     for(let elemento1 of adj)
+//     {
+//         for(let elemento2 of noun)
+//             {for(let elemento3 of dominio)
+//                 {
+//                 console.log(elemento+ elemento1 + elemento2 + elemento3);
+//                 };
+        
+//         };
+//     };
+// };
